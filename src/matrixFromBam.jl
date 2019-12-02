e8a362851772d3f241e23499446cec2a9d6d4653
GarrettJenkinson <gargar934@gmail.com>
2019-12-01 22:40:27 -0600

    escape $ in docstring

--- src/matrixFromBam.jl
+++ src/matrixFromBam.jl
@@ -30,7 +30,7 @@
  is methylated.
 
  This function depends on a working instalation of SAMtools that is on
- the system path $PATH.
+ the system path \$PATH.
 
  Before running this function, FastaToCpG.m must be run ONCE.
 
