f1fc3a5d2ae5a65c0835679de37d782a8d5298d9
GarrettJenkinson <gargar934@gmail.com>
2019-12-01 18:15:38 -0600

    updatind docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,22 @@ using Documenter, InformMe
 
 makedocs(;
     modules=[InformMe],
-    format=Documenter.HTML(),
+    format=:html,
+    source="src",
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/GarrettJenkinson/InformMe.jl/blob/{commit}{path}#L{line}",
     sitename="InformMe.jl",
-    authors="GarrettJenkinson"
+    authors="Garrett Jenkinson"
 )
 
 deploydocs(;
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
+    branch = "gh-pages",
+    latest = "master",
+    target="build",
+    deps = nothing,
+    make = nothing,
     repo = "github.com/GarrettJenkinson/InformMe.jl",
     julia = "1.3",
     osname = "linux"
